b684e59a40c11354fb560d4a059e59b0986c01b5
Shobhit Umrao <shobhitumrao5@gmail.com>
2024-03-29 17:36:27 +0530

    subd

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,17 +11,27 @@ async function middleware(req: NextRequest) {
   // Clone the URL
   const url = req.nextUrl.clone();
 
+  //   console.log("Incoming request URL:", url.toString());
+
   // Skip public files
-  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes("_next")) return;
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes("_next")) {
+    // console.log("Skipping public file or Next.js internal file.");
+    return;
+  }
 
   const host = req.headers.get("host");
+  console.log("Incoming request host:", host);
+
   const subdomain = getValidSubdomain(host);
+  console.log("Detected subdomain:", subdomain);
 
   if (subdomain) {
     // Subdomain available, appending as query parameter
     url.searchParams.set("subdomain", subdomain);
+    console.log("Subdomain parameter added to URL:", url.toString());
   }
 
+  console.log("Rewriting URL:", url.toString());
   return NextResponse.rewrite(url);
 }
 
