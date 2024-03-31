a35b9369b7dfc8fa28eb2283d248f95542558087
Shobhit Umrao <shobhitumrao5@gmail.com>
2024-03-31 21:58:12 +0530

    ui changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,14 @@ export default function Home(props: any) {
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Get started by editing&nbsp; <br />
-            subdomain:&nbsp;{subdomain}
-            ,<br />
+        <div className="z-10 text-xl  w-full items-center justify-between font-mono lg:flex">
+          <p className="w-96  flex  justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <p className="">
+              Try a different subdomain&nbsp; <br />
+            </p>
+            subdomain:&nbsp;{subdomain},<br />
             templateId:&nbsp;{templateId}, <br />
             videoId:&nbsp;{videoId}
-            <code className="font-mono font-bold">src/pages/index.tsx</code>
           </p>
         </div>
       </main>
